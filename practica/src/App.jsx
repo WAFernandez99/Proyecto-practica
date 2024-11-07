1715ff839ca0de0e7d2df06f7864665f4017b9f4
WAFernandez99 <walterarielfernandez730@gmail.com>
2024-11-07 13:59:22 -0300

    Cambios de tipo de archivos y comienzo del programa

---
+++ practica/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import header from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <FormAddMoney />
+      <h2>Walter's components</h2>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ practica/src/components/FormAddMoney.jsx
@@ -0,0 +1,13 @@
+const FormAddMoney = () => {
+    return(
+<div className="form add money">
+    <form action="">
+        <p></p>
+        <input type="number" placeholder="300$"/>
+        <input type="submit" value="Agregar" />
+    </form>
+</div>
+    );
+}
+
+export default FormAddMoney;
