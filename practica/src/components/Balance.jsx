32e8a79341d06e31eefb98924a8fa5f07c9d9f28
WAFernandez99 <walterarielfernandez730@gmail.com>
2024-11-12 15:36:19 -0300

    Agregar algo para github

--- practica/src/components/Balance.jsx
+++ practica/src/components/Balance.jsx
@@ -4,6 +4,7 @@ const Balance = ({count}) =>{
             <h3>Presupuesto: {count}</h3>
             <h3>Disponible: {count}</h3>
             <h3>Gastado: {count}</h3>
+            <h3>Total:{count}</h3>
         </div>
     );
 }
